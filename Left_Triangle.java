701bdc9f546abf25758560c75a88d49d74443fc9
BingiGuravaiah <bingiguravaiah7259513406@gmail.com>
2024-08-20 22:39:54 +0530

    Create Left_Triangle.java
    Write a java program to print the Left increasing triangle pattern
        *
       **
      ***
     ****

---
+++ Left_Triangle.java
@@ -0,0 +1,17 @@
+class pattern2
+{
+public static void main(String args[])
+{
+ for(int i=0;i<=4;i++)
+{
+for(int j=4-i;j>=0;j--)   //Dreasing tringle program logic
+{
+  System.out.print(" "); 
+} for(int j=0;j<i;j++)    //inceasing tringle program logic
+{
+  System.out.print("*");
+}
+ System.out.println();
+}
+}
+}
