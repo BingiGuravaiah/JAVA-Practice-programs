8a58949abe2bc4dec9b631040dab167fe5b4ee9c
BingiGuravaiah <bingiguravaiah7259513406@gmail.com>
2024-08-21 07:27:10 +0530

    Create Reversing_Number.java
    How to Reverse a Number in Java

---
+++ Reversing_Number.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number: ");
+        int num=sc.nextInt();
+        int sum=0;
+        while(num!=0){
+            int temp=num%10;
+            sum=temp+(sum*10);
+            num=num/10;
+         }
+         System.out.println(sum);
+    }
+}
