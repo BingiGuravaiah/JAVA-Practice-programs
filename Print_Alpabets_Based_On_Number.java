ef9883b10464d11fb2711b4da11108521f46ba0a
BingiGuravaiah <bingiguravaiah7259513406@gmail.com>
2024-08-30 12:43:40 +0530

    Create Print_Alpabets_Based_On_Number.java
    INPUT: a1bc5
    OUTPUT: abcbcbcbcbc

---
+++ Print_Alpabets_Based_On_Number.java
@@ -0,0 +1,45 @@
+import java.util.*;
+public class Print_Alpabets_Based_On_Number {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the String: ");
+        String str=sc.next();
+        String temp="";
+        String temp2="";
+        ArrayList<Integer> all=new ArrayList<>();
+        ArrayList<String> am=new ArrayList<>();
+        for(int i=0;i<str.length();i++){
+            char ch=str.charAt(i);
+            if(Character.isAlphabetic(ch)){
+                temp2+=ch;
+            }else{
+                am.add(temp2);
+                temp2="";
+
+            }
+        }
+        for(int i=0;i<str.length();i++){
+            char ch=str.charAt(i);
+            if(Character.isDigit(ch)){
+                temp+=ch;
+            }else{
+                if(temp!=""){
+                int m=Integer.valueOf(temp);
+                all.add(m);
+                temp="";
+                }
+            }
+        }
+        if(temp!=""){
+            int m=Integer.valueOf(temp);
+            all.add(m);
+        }
+        for(int i=0;i<all.size();i++){
+            int x=all.get(i);
+            for(int j=0;j<x;j++){
+                System.out.print(am.get(i));
+            }
+        }
+        
+        }
+    }
